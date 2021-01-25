b2229fc0e617f5038324eab9455887f2c928ed8f
Anne Glines <anneglines@gmail.com>
2021-01-24 22:21:07 -0800

    add form and submit logic

--- src/core/donation-form/index.tsx
+++ src/core/donation-form/index.tsx
@@ -3,11 +3,22 @@ import { DonateButton } from './donate-button'
 import { DonateInput } from './donate-input'
 import styles from './styles.scss'
 export const DonationForm: React.FC = () => {
+  const [totalValue, setTotalValue] = React.useState(0)
+  const [totalDonators, setTotalDonators] = React.useState(0)
   const [donateValue, setDonateValue] = React.useState('50')
   const handleValueChange = React.useCallback((value: string) => {
     setDonateValue(value)
   }, [])
 
+  const handleSubmit = React.useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault()
+        setTotalValue(value => value + parseInt(donateValue, 10))
+        setTotalDonators(donators => donators + 1)
+      }
+    },
+    [donateValue]
+  )
   return (
     <div className={styles.wrapper}>
       <div className={styles.donateForm}>
@@ -19,11 +30,15 @@ export const DonationForm: React.FC = () => {
             Join the <strong>11</strong> other donors who have already supported
             this project.
           </p>
+          <form
+            onSubmit={handleSubmit}
+          >
             <DonateInput
               value={donateValue}
               handleValueChange={handleValueChange}
             />
             <DonateButton />
+          </form>
         </div>
       </div>
     </div>
